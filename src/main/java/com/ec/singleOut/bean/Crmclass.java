b76a1357bc0886055d3bbeeb1620d2e1d3863770
xiexg <568335129@qq.com>
2016-01-28 15:16:13 +0800

    fix bug, 标签bug

---
+++ src/main/java/com/ec/singleOut/bean/Crmclass.java
@@ -0,0 +1,18 @@
+package com.ec.singleOut.bean;
+
+/**
+ * Created by ecuser on 2016/1/28.
+ */
+public class Crmclass {
+
+    public long classId;
+
+    public long  getClassId() {
+        return classId;
+    }
+
+    public Crmclass setClassId(long  classId) {
+        this.classId = classId;
+        return this;
+    }
+}
